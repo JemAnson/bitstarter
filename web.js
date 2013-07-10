4e388b3a38d2bf374f50725e35ffff3474743b4d
Jeremy Anson <jemanson@gmail.com>
2013-07-10 20:12:05 +0000

    go

--- web.js
+++ web.js
@@ -1,14 +1,16 @@
 
-var buf= new buffer(256);
-buf = fs.readFileSync("index.html");
-var data = buf.toString('utf8',0,buf.length);
+//var buf= new buffer(256);
+//buf = fs.readFileSync("index.html");
+//var data = buf.toString('utf8',0,buf.length);
+var data = "Hello";
+
 alert(data);
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-//  response.send(data);
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
