6807a92e239d61e3fcdeae6dc8e22c843180ee87
Jeremy Anson <jemanson@gmail.com>
2013-07-10 20:01:28 +0000

    last go

--- web.js
+++ web.js
@@ -1,13 +1,14 @@
 
 var buf= new buffer(256);
 buf = fs.readFileSync("index.html");
-
+var data = buf.toString('utf8',0,buf.length);
+alert(data);
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString('utf8',0,len));
+//  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
